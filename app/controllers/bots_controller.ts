3fcc3dc3ea8c0c3d031434b5a14d362707c2443e
AisAif <Isbahrulfahni@gmail.com>
2025-01-12 13:14:02 +0700

    chore: setup s3 drive

--- app/controllers/bots_controller.ts
+++ app/controllers/bots_controller.ts
@@ -1,7 +1,14 @@
 import type { HttpContext } from '@adonisjs/core/http'
+import drive from '@adonisjs/drive/services/main'
+import env from '#start/env'
 
 export default class BotsController {
   public async index({ inertia }: HttpContext) {
-    return inertia.render('dashboard/bot/index')
+    const result = await drive.use('s3').listAll(env.get('NODE_ENV'))
+    const models = []
+    for (const model of result.objects) {
+      models.push(model)
+    }
+    return inertia.render('dashboard/bot/index', { models })
   }
 }
--- inertia/pages/dashboard/bot/index.tsx
+++ inertia/pages/dashboard/bot/index.tsx
@@ -1,7 +1,10 @@
 import { AppSidebar } from '~/components/app-sidebar'
 import { SidebarInset, SidebarProvider } from '~/components/ui/sidebar'
+import type { InferPageProps } from '@adonisjs/inertia/types'
+import BotsController from '#controllers/bots_controller'
 
-export default function Page() {
+export default function Page({ models }: InferPageProps<BotsController, 'index'>) {
+  console.log(models)
   return (
     <SidebarProvider>
       <AppSidebar />
